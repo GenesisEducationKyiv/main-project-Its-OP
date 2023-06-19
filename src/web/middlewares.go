6689a6947106ddf873b57985334a7b74f2c5dbd4
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-06-19 12:17:30 +0300

    Remove comments from middleware

--- src/web/middlewares.go
+++ src/web/middlewares.go
@@ -9,12 +9,10 @@ import (
 
 func errorHandlingMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Next() // execute the next middleware or handler
+		c.Next()
 
-		// Check if there was an error
 		if len(c.Errors) > 0 {
 			for _, e := range c.Errors {
-				// Check if the error is a CustomError
 				if _, ok := e.Err.(*domain.EndpointInaccessibleError); ok {
 					c.String(http.StatusBadRequest, e.Error())
 				} else if _, ok := e.Err.(*domain.DataConsistencyError); ok {
