6bdd15809774172ce6f705538d9116cf3b952b26
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-06-24 22:55:02 +0300

    Implement SupportedCurrencyValidator

--- src/application/validators/supported_currency_validator.go
+++ src/application/validators/supported_currency_validator.go
@@ -1,1 +1,11 @@
 package validators
+
+import "golang.org/x/exp/slices"
+
+type SupportedCurrencyValidator struct {
+	supportedCurrencies []string
+}
+
+func (v *SupportedCurrencyValidator) Validate(currency string) (bool, error) {
+	return slices.Contains(v.supportedCurrencies, currency), nil
+}
