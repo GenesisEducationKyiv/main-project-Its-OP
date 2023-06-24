c2d543d23ef7987d301933bf8659d90fc92b909c
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-06-24 22:55:13 +0300

    Implement SupportedCoinValidator

--- src/application/validators/supported_coin_validator.go
+++ src/application/validators/supported_coin_validator.go
@@ -1,1 +1,11 @@
 package validators
+
+import "golang.org/x/exp/slices"
+
+type SupportedCoinValidator struct {
+	supportedCoins []string
+}
+
+func (v *SupportedCoinValidator) Validate(coin string) (bool, error) {
+	return slices.Contains(v.supportedCoins, coin), nil
+}
