6b2130e9afa7851fabb87aa2e29e01a28a0dfa89
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-06-24 22:52:10 +0300

    Implement emailValidator

--- src/application/validators/email_validator.go
+++ src/application/validators/email_validator.go
@@ -1,1 +1,16 @@
 package validators
+
+import "regexp"
+
+type EmailValidator struct{}
+
+func (e *EmailValidator) Validate(email string) (bool, error) {
+	regexString := "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+
+	match, err := regexp.Match(regexString, []byte(email))
+	if err != nil {
+		return false, err
+	}
+
+	return match, nil
+}
