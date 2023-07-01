1591f2bf1e279fc03d3d012d51badaaf60a4ffed
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-07-01 21:07:17 +0300

    Add Response

--- src/infrastructure/integrations/contracts.go
+++
@@ -1,9 +0,0 @@
-package integrations
-
-import "net/http"
-
-type IExtendedHttpClient interface {
-	SendRequest(req *http.Request) ([]byte, int, error)
-}
-
-const endpointInaccessibleErrorMessage = "Couldn't access any of the supported providers"
