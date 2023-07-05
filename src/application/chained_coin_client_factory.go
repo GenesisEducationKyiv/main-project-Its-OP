237ee099dabeba4011894ef683b918e7ceee9840
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-07-05 20:14:58 +0300

    Create ChainedClientFactory

---
+++ src/application/chained_coin_client_factory.go
@@ -0,0 +1,27 @@
+package application
+
+import "btcRate/application/services"
+
+type ChainedCoinClientFactory struct {
+	factories []services.ICoinClientFactory
+}
+
+func (*ChainedCoinClientFactory) NewChainedCoinClientFactory(cfs []services.ICoinClientFactory) *ChainedCoinClientFactory {
+	return &ChainedCoinClientFactory{factories: cfs}
+}
+
+func (f *ChainedCoinClientFactory) CreateClient() services.ICoinClient {
+	if len(f.factories) == 0 {
+		return nil
+	} else if len(f.factories) == 1 {
+		return f.factories[0].CreateClient()
+	}
+
+	lastChainedClient := newChainedCoinClientProxy(f.factories[len(f.factories)-1].CreateClient(), nil)
+	for i := len(f.factories) - 2; i >= 0; i-- {
+		chainedClient := newChainedCoinClientProxy(f.factories[i].CreateClient(), lastChainedClient)
+		lastChainedClient = chainedClient
+	}
+
+	return lastChainedClient
+}
