3c459e891a780323709218cfc981f568dc6a950f
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-07-08 13:12:32 +0300

    Implement base of RateProvider

---
+++ src/campaign/infrastructure/providers/rate_provider.go
@@ -0,0 +1,24 @@
+package providers
+
+import (
+	"campaign/domain"
+	"campaign/infrastructure"
+	"net/url"
+	"time"
+)
+
+type RateProvider struct {
+	client  infrastructure.IHttpClient
+	baseURL *url.URL
+}
+
+func NewRateProvider(client infrastructure.IHttpClient) *RateProvider {
+	// TODO: set the url of the feature-coin container
+	baseUrl := &url.URL{Host: "coin", Path: "path"}
+	return &RateProvider{client: client, baseURL: baseUrl}
+}
+
+func (*RateProvider) GetRate() (domain.Rate, error) {
+	// TODO: implement proper rate fetching
+	return domain.Rate{Amount: 1000, Timestamp: time.Now()}, nil
+}
