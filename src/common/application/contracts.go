31849ddac0a4de41c344702bb422fe642c74050c
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-07-12 16:26:36 +0300

    Add ILogger

--- src/common/application/contracts.go
+++ src/common/application/contracts.go
@@ -3,3 +3,9 @@ package application
 type IValidator[T any] interface {
 	Validate(T) error
 }
+
+type ILogger interface {
+	LogInformation(message string) error
+	LogDebug(message string) error
+	LogError(err error, message string) error
+}
