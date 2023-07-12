5c987acdc87bfc9f50b977834e723c3e14fcbc29
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-07-12 16:26:49 +0300

    Use ILogger in LoggedHttpClient

--- src/common/infrastructure/extensions/logged_http_client.go
+++ src/common/infrastructure/extensions/logged_http_client.go
@@ -1,6 +1,7 @@
 package extensions
 
 import (
+	"btcRate/common/application"
 	"btcRate/common/infrastructure"
 	"fmt"
 	"net/http"
@@ -13,11 +14,11 @@ type ILogRepository interface {
 
 type LoggedHttpClient struct {
 	httpClient infrastructure.IHttpClient
-	repository ILogRepository
+	logger     application.ILogger
 }
 
-func NewLoggedHttpClient(httpClient infrastructure.IHttpClient, repository ILogRepository) *LoggedHttpClient {
-	return &LoggedHttpClient{httpClient: httpClient, repository: repository}
+func NewLoggedHttpClient(httpClient infrastructure.IHttpClient, logger application.ILogger) *LoggedHttpClient {
+	return &LoggedHttpClient{httpClient: httpClient, logger: logger}
 }
 
 func (c *LoggedHttpClient) SendRequest(req *http.Request) (*infrastructure.HttpResponse, error) {
@@ -27,22 +28,19 @@ func (c *LoggedHttpClient) SendRequest(req *http.Request) (*infrastructure.HttpR
 	resp, err := c.httpClient.SendRequest(req)
 
 	var logMessage string
+	var logErr error
 
 	if err != nil {
-		logMessage = fmt.Sprintf("%s,%s,error: '%s'", timestamp.Format("02-01-06 15:04:05.999 Z0700"), url, err.Error())
+		logMessage = fmt.Sprintf("%s,%s", timestamp.Format("02-01-06 15:04:05.999 Z0700"), url)
+		logErr = c.logger.LogError(err, logMessage)
 	} else {
 		logMessage = fmt.Sprintf("%s,%s,%d,'%s'", timestamp.Format("02-01-06 15:04:05.999 Z0700"), url, resp.Code, string(resp.Body))
+		logErr = c.logger.LogDebug(logMessage)
 	}
-
-	logErr := c.repository.Log(logMessage)
 
 	if logErr != nil {
-		fmt.Printf("error: failed to save the log. %f %s", err, logMessage)
+		fmt.Printf(err.Error())
 	}
 
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
+	return resp, err
 }
