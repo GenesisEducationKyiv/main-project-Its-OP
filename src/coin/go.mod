bcf940a51454c5a3e765a9f98739ea8cadf6fb78
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-07-16 14:14:57 +0300

    Register RabbitMQ commandBus

--- src/coin/go.mod
+++ src/coin/go.mod
@@ -4,9 +4,11 @@ go 1.20
 
 require (
 	btcRate/common v0.0.0-00010101000000-000000000000
+	github.com/ThreeDotsLabs/watermill v1.3.2
+	github.com/ThreeDotsLabs/watermill-amqp/v2 v2.1.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/goccy/go-json v0.10.2
-	github.com/stretchr/testify v1.8.3
+	github.com/stretchr/testify v1.8.4
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.1
@@ -16,6 +18,7 @@ require (
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
+	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
@@ -27,16 +30,24 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rabbitmq/amqp091-go v1.2.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
--- src/coin/main.go
+++ src/coin/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"btcRate/coin/web"
+	"btcRate/common/infrastructure/bus/command_handlers"
+	"context"
+	"github.com/ThreeDotsLabs/watermill-amqp/v2/pkg/amqp"
+	"github.com/ThreeDotsLabs/watermill/components/cqrs"
+	"github.com/ThreeDotsLabs/watermill/message"
 	"log"
 	"os"
 	"os/signal"
 )
 
 func main() {
+	commandBus := addCommandBus()
 	server := web.NewServerManager()
 	stop, err := server.RunServer("./logs/coin-logs.csv")
 	if err != nil {
@@ -20,4 +26,66 @@ func main() {
 	if err := stop(); err != nil {
 		log.Fatal("Failed to stop the server: ", err)
 	}
+}
+
+func addCommandBus() *cqrs.CommandBus {
+	cqrsMarshaler := cqrs.ProtobufMarshaler{}
+	commandsAMQPConfig := amqp.NewDurableQueueConfig("amqp://localhost:5672")
+
+	commandsPublisher, err := amqp.NewPublisher(commandsAMQPConfig, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	commandsSubscriber, err := amqp.NewSubscriber(commandsAMQPConfig, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	router, err := message.NewRouter(message.RouterConfig{}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	commandBus, err := cqrs.NewCommandBusWithConfig(
+		commandsPublisher,
+		cqrs.CommandBusConfig{
+			GeneratePublishTopic: func(params cqrs.CommandBusGeneratePublishTopicParams) (string, error) {
+				return params.CommandName, nil
+			},
+			Marshaler: cqrsMarshaler,
+		})
+	if err != nil {
+		panic(err)
+	}
+
+	commandProcessor, err := cqrs.NewCommandProcessorWithConfig(
+		router,
+		cqrs.CommandProcessorConfig{
+			GenerateSubscribeTopic: func(params cqrs.CommandProcessorGenerateSubscribeTopicParams) (string, error) {
+				return params.CommandName, nil
+			},
+			SubscriberConstructor: func(params cqrs.CommandProcessorSubscriberConstructorParams) (message.Subscriber, error) {
+				return commandsSubscriber, nil
+			},
+			Marshaler: cqrsMarshaler,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	err = commandProcessor.AddHandlers(
+		command_handlers.LogCommandHandler{},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// processors are based on router, so they will work when router will start
+	if err := router.Run(context.Background()); err != nil {
+		panic(err)
+	}
+
+	return commandBus
 }
